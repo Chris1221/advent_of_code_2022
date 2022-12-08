76d4e4ae474c030913556b082529d8134788ac15
Christopher Cole <christopher@deepgenomics.com>
2022-12-08 15:35:30 -0500

    day 8

--- advent_of_code/day_8/challenges.py
+++ advent_of_code/day_8/challenges.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 FILE: str = "data/day_8/input.txt"
+# FILE: str = "data/day_8/test.txt"
 
 
 def tallest_from(df, direction, i, j, v):
@@ -13,36 +14,33 @@ def tallest_from(df, direction, i, j, v):
     if direction == "top":
         # Count down the rows of the j column
         for row_counter in range(i):
-            if df[j][row_counter] >= v:
+            if int(df[j][row_counter]) >= v:
                 return False
 
     if direction == "bottom":
         # Count the opposite direction from the bottom
-
-        for row_counter in reversed(range(i, df.shape[0])):
-
-            if df[j][row_counter] >= v:
+        for row_counter in reversed(range(i + 1, df.shape[0])):
+            if int(df[j][row_counter]) >= v:
                 return False
 
     if direction == "right":
 
-        for col_counter in reversed(range(j, df.shape[1])):
+        for col_counter in reversed(range(j + 1, df.shape[1])):
 
-            if df[i][col_counter] >= v:
+            if int(df[col_counter][i]) >= v:
                 return False
 
     if direction == "left":
 
-        for col_counter in range(j, df.shape[1]):
+        for col_counter in range(j):
 
-            if df[i][col_counter] >= v:
+            if int(df[col_counter][i]) >= v:
                 return False
 
     return True
 
 
 def challenge_1(file=FILE):
-
     # Read the trees into a numpy matrix, then
     # iterate for each i and j to see if that i, j
     # is the max element in the column up, down, side, and side
@@ -56,14 +54,63 @@ def challenge_1(file=FILE):
     for i in range(df.shape[0]):
         for j in range(df.shape[1]):
             df_index.loc[i, j] = (
-                tallest_from(df, "top", i, j, df.loc[i, j])
-                or tallest_from(df, "bottom", i, j, df.loc[i, j])
-                or tallest_from(df, "right", i, j, df.loc[i, j])
-                or tallest_from(df, "left", i, j, df.loc[i, j]),
+                tallest_from(df, "top", i, j, int(df.loc[i, j]))
+                or tallest_from(df, "bottom", i, j, int(df.loc[i, j]))
+                or tallest_from(df, "right", i, j, int(df.loc[i, j]))
+                or tallest_from(df, "left", i, j, int(df.loc[i, j]))
             )
 
-    sdfasdf
+    print(f"The total number of trees visible is {df_index.sum().sum()}")
+
+
+def scenic_score(df, i, j):
+
+    # For position i, j
+
+    # First look up
+    # If it's from the top, we want to see if its the
+    # Count down the rows of the j column
+
+    v = int(df.loc[i, j])
+
+    up_score = 0
+    for row_counter in reversed(range(i)):
+        up_score += 1
+        if int(df[j][row_counter]) >= v:
+            break
+
+    down_score = 0
+    for row_counter in range(i + 1, df.shape[0]):
+        down_score += 1
+        if int(df[j][row_counter]) >= v:
+            break
+
+    right_score = 0
+    for col_counter in range(j + 1, df.shape[1]):
+        right_score += 1
+        if int(df[col_counter][i]) >= v:
+            break
+
+    left_score = 0
+    for col_counter in reversed(range(j)):
+        left_score += 1
+        if int(df[col_counter][i]) >= v:
+            break
+
+    if i == 1 and j == 2:
+        breakpoint()
+
+    return up_score * down_score * right_score * left_score
 
 
 def challenge_2(file=FILE):
-    pass
+    lines = [i.strip() for i in open(file).readlines()]
+    df = pd.DataFrame([list(i) for i in lines])
+
+    df_index = pd.DataFrame(np.zeros(df.shape))
+
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            df_index.loc[i, j] = scenic_score(df, i, j)
+
+    print(f"The max is {df_index.max().max()}")
