4b9e85dc6661f73373a8f8be6c6596e2778ee47a
Christopher Cole <ccole@well.ox.ac.uk>
2022-12-09 22:23:32 -0500

    day 9

--- advent_of_code/day_9/challenges.py
+++ advent_of_code/day_9/challenges.py
@@ -16,53 +16,25 @@ def euc_dist(t, h):
     return sqrt((h[0] - t[0]) ** 2 + (h[1] - t[1]) ** 2)
 
 
-def get_new_pos(tail_pos, head_pos, direction):
-
-    # Incremement head_pos by direction
-    new_head_pos = (
-        head_pos[0] + DIRS[direction][0],
-        head_pos[1] + DIRS[direction][1],
-    )
-
-    # Check if tail position is lagging
-
-    # Check if an additional diagonal move
-    # is needed
-
+def get_tail_pos(tail_pos, new_head_pos):
     y_extra = 0
     x_extra = 0
     if euc_dist(tail_pos, new_head_pos) > 2:
-        # whichever direction it moved in
-        # add one to the other direction
-
         if abs(new_head_pos[0] - tail_pos[0]) > 1:
             y_extra = new_head_pos[1] - tail_pos[1]
         if abs(new_head_pos[1] - tail_pos[1]) > 1:
             x_extra = new_head_pos[0] - tail_pos[0]
+
+        if (y_extra != 0) and (x_extra != 0):
+            y_extra = 0
+            x_extra = 0
 
     new_tail_pos = (
         diff(tail_pos[0] + x_extra, new_head_pos[0]),
         diff(tail_pos[1] + y_extra, new_head_pos[1]),
     )
 
-    # head_positions.append(new_head_pos)
-    # tail_positions.append(new_tail_pos)
-
-    # if tail_pos == (2, 4):
-    #    breakpoint()
-
-    # print(
-    #     f"""
-    # The instruction was {line}:
-    #     Old_positions: {head_pos}, {tail_pos}
-    #     New positions: {new_head_pos}, {new_tail_pos}
-    # """
-    # )
-
-    # head_pos = new_head_pos
-    # tail_pos = new_tail_pos
-
-    return new_tail_pos, new_head_pos
+    return new_tail_pos
 
 
 def challenge_1(file=FILE):
@@ -77,9 +49,12 @@ def challenge_1(file=FILE):
 
     for line in lines:
         direction, moves = line.split(" ")
-
         for move in range(int(moves)):
-            tail_pos, head_pos = get_new_pos(tail_pos, head_pos, direction)
+            head_pos = (
+                head_pos[0] + DIRS[direction][0],
+                head_pos[1] + DIRS[direction][1],
+            )
+            tail_pos = get_tail_pos(tail_pos, head_pos)
             head_positions.append(head_pos)
             tail_positions.append(tail_pos)
 
@@ -87,30 +62,25 @@ def challenge_1(file=FILE):
 
 
 def challenge_2(file=FILE):
-
     lines = [i.strip() for i in open(file).readlines()]
-
-    tail_pos = [(0, 0) for i in range(10)]
-    head_pos = (0, 0)
-
-    head_positions = [head_pos]
-    tail_positions = [knots]
+    pos = [(0, 0) for i in range(10)]
+    tail_positions = []
 
     for line in lines:
         direction, moves = line.split(" ")
 
         for move in range(int(moves)):
+            new_pos = []
+            new_pos.append(
+                (
+                    pos[0][0] + DIRS[direction][0],
+                    pos[0][1] + DIRS[direction][1],
+                )
+            )
+            for i in range(1, len(pos)):
+                new_pos.append(get_tail_pos(pos[i], new_pos[i - 1]))
 
-            # First get the new head position like normal
+            tail_positions.append(new_pos[-1])
+            pos = new_pos
 
-            new_tail_pos, new_head_pos = get_new_pos(tail_pos[0], head_pos, direction)
-            new_tail_pos = [new_tail_pos]
-            # And then go through the list
-
-            for idx, cur_tail_pos in enumerate(tail_pos[1:]):
-                current_new_tail_post, new_
-
-                head_positions.append(head_pos)
-                tail_positions.append(tail_pos)
-
-    pass
+    print(f"A total of {len(set(tail_positions))} were visited")
